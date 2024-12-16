5eaa34843cb97913c4a2f2a4d115dc48076694bb
BT_2S14_04 <e@gmail.com>
2024-12-16 10:58:23 -0500

    Made some adjustments regarding time changes

--- src/Time.java
+++ src/Time.java
@@ -1,35 +1,56 @@
+/**
+ * This Time class represents an analog clock. The clock displays hours, minutes, and seconds.
+ */
 public class Time {
     public int secs;
     public int mins;
     public int hours;
 
-
+    /**
+     *  Constructor for the Time class. This creates a new instance of a clock given the below parameters.
+     * @param hours represents the hour.
+     * @param mins represents the minutes.
+     * @param secs represents the seconds.
+     */
     Time(int hours, int mins, int secs){
         this.hours = hours;
         this.mins = mins;
         this.secs = secs;
     }
 
+    /**
+     * tick method for the Time class. Adds one second to an instance of this class.
+     */
     public void tick(){
         secs+=1;
     }
 
+    /**
+     * add method for the Time class. Adds the time from one instance of the class to another.
+     * @param time represents an instance of the class, containing hours, minutes, and seconds.
+     */
     public void add(Time time){
         hours += time.hours;
         mins += time.mins;
         secs += time.secs;
     }
 
-public String toString(){
+    /**
+     * toString method for the Time class. It checks whether the seconds, minutes, or hours exceed
+     * a logical display on a time. If so, it adjusts it accordingly. It also checks  if any time
+     * value is one digit, which then adds a 0 prior to it.
+     * @return returns the time
+     */
+    public String toString(){
         String seconds;
         String minutes;
         String hour;
         if (secs>= 60){
-            secs = 0;
+            secs = secs-60;
             mins +=1;
         }
         if (mins>= 60){
-            mins = 0;
+            mins = mins-60;
             hours +=1;
         }
         if (hours>=24){
